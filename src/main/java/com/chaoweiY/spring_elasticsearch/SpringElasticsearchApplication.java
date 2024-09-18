b00b85dfb472e2d9edf80600987951aa730a5679
ycw <2562424639@qq.com>
2024-09-18 17:58:03 +0800

    restcontroller

--- src/main/java/com/chaoweiY/spring_elasticsearch/SpringElasticsearchApplication.java
+++ src/main/java/com/chaoweiY/spring_elasticsearch/SpringElasticsearchApplication.java
@@ -2,7 +2,10 @@ package com.chaoweiY.spring_elasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class SpringElasticsearchApplication {
 
