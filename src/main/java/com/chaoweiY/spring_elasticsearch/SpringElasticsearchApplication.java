742b816174431ad4e1b468ee82f419a77b38044c
ycw <2562424639@qq.com>
2024-09-18 17:40:23 +0800

    first commit

---
+++ src/main/java/com/chaoweiY/spring_elasticsearch/SpringElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.chaoweiY.spring_elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chaoweiY/spring_elasticsearch/SpringElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaoweiY.spring_elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
